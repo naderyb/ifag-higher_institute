4770a82c3fe4a8f20cf0dbd2a7f438384ddac599
Nader youb <youb.nader@gmail.com>
2025-03-31 02:23:25 +0100

    Add scroll behavior to router for smooth navigation

--- src/router/index.js
+++ src/router/index.js
@@ -64,7 +64,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
+});
 
 export default router
